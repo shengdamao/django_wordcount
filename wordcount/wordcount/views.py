7914615548c0416f1e4b50e0c232d795823d42e7
Shengda <39946188+shengdamao@users.noreply.github.com>
2019-12-22 18:54:22 +0800

    about.html
    about.html

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -17,3 +17,7 @@ def count(request):
         worddict[word] = worddict.get(word, 0) + 1
     return render(request, 'count.html', {'fulltext': fulltext, 'wordcnt': wordcnt, 'worddict': worddict,
                                           'items': worddict.items()})
+
+def about(request):
+
+    return render(request, 'about.html')
