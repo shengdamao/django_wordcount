a4af38f8d384791bef06836314fa00e2fa2dc3aa
Shengda <39946188+shengdamao@users.noreply.github.com>
2019-12-22 17:12:09 +0800

    wordcount-mainlogic
    wordcount-mainlogic

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -5,6 +5,15 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'home.html', {'key1':'value1'})
 
-def count(request):
-    return render(request, 'count.html')
 
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlst = fulltext.split()
+    wordcnt = len(wordlst)
+
+    worddict = dict()
+    for word in wordlst:
+        worddict[word] = worddict.get(word, 0) + 1
+    return render(request, 'count.html', {'fulltext': fulltext, 'wordcnt': wordcnt, 'worddict': worddict,
+                                          'items': worddict.items()})
